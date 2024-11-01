5b37b372dcef58e1d2192859213fff3970789d11
yodemisj <yodemisj@gmail.com>
2024-11-01 16:33:43 -0300

    chore: prisma configuration

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import route from './routes/route';
+import prisma from './prisma';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,6 +10,24 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/', route);
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
-})
+});
+
+process.on('SIGINT', async () => {
+    console.log('Closing Prisma connection...');
+    await prisma.$disconnect();
+    server.close(() => {
+        console.log('Server has been shut down.');
+        process.exit(0);
+    });
+});
+
+process.on('SIGTERM', async () => {
+    console.log('Closing Prisma connection...');
+    await prisma.$disconnect();
+    server.close(() => {
+        console.log('Server has been shut down.');
+        process.exit(0);
+    });
+});

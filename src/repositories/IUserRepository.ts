f12b85e9a55304b75e079da13539308794476f92
yodemisj <junior.nascimento@fiscal.io>
2025-02-16 23:59:09 -0300

    remove obsolete files

--- src/repositories/IUserRepository.ts
+++
@@ -1,9 +0,0 @@
-import { User } from "../entities/User";
-
-export interface IUserRepository {
-    create(user: User): Promise<void>;
-    read(): Promise<User[]>;
-    update(user: User): Promise<void>;
-    delete(id: number): Promise<User>;
-    find(id: number): Promise<User>;
-}

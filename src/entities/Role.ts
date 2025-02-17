a59bc52cadfb6211fda1b0cda2037377953df416
yodemisj <junior.nascimento@fiscal.io>
2025-02-16 23:40:32 -0300

    refact: refactoring Role entity

--- src/entities/Role.ts
+++ src/entities/Role.ts
@@ -1,6 +1,28 @@
+import { v4 as uuidv4 } from "uuid";
+
 export class Role {
-    constructor(
-        public id: string,
-        public name: string
-    ) {}
+    private _id: string;
+    private _name!: string;
+
+    constructor( name: string, id?: string ) {
+        this._id = id ?? uuidv4();
+        this.name = name;
+    }
+
+    public get id() {
+        return this._id;
+    }
+
+    public get name() {
+        return this._name;
+    }
+
+    public set name(value: string) {
+        if(value.length < 3) {
+            throw new Error("The name must have at least 3 characters.");
+        } else if(value.length > 150) {
+            throw new Error("The name must have a maximum of 150 characters.");
+        }
+        this._name = value;
+    }
 }

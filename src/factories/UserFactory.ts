aea29b45e0a55f055b064fe5fda1dfd762df112d
yodemisj <junior.nascimento@fiscal.io>
2025-02-16 23:56:36 -0300

    feat: add user factory

--- src/factories/UserFactory.ts
+++ src/factories/UserFactory.ts
@@ -1,9 +1,15 @@
 import prisma from "../prisma";
-import { UserRepository } from "../repositories/UserRepository";
+import { UserControllerExpress } from "../controllers/user/UserController.express";
+import { RoleRepositoryPrisma } from "../repositories/role/RoleRepository.prisma";
+import { UserRepositoryPrisma } from "../repositories/user/UserRepository.prisma";
+import { UserService } from "../services/user/UserService";
 
 export class UserFactory {
     static getInstance() {
-        const userRepository = new UserRepository(prisma);
-        return userRepository;       
+        const userRepository = new UserRepositoryPrisma(prisma);
+        const roleRepository = new RoleRepositoryPrisma(prisma);
+        const userService = new UserService(userRepository, roleRepository);
+        const userController = new UserControllerExpress(userService);
+        return userController;       
     }
 }
